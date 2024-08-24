6817c753adaa2d71ed77e5cb00819a8b9d99ac98
PatoFlamejanteTV <flamejantepato77@gmail.com>
2024-08-24 10:27:45 -0300

    teste_agora_com_espaços_aaaa____

--- push.bat
+++ push.bat
@@ -1,6 +1,10 @@
 git add *
 git push
-git commit -m "%1"
+git commit -m "%1_%2_%3_%4_%5_%6_%7_%8_%9"
+:: "hack" HORRIVEL, tenho vergonha de ter programado isso,
+::				mas não arrependimento ;)
 echo GIT STATUS:
 git status
 pause
+:: odeio git odeio tudo odeio todos odeio a umanidade mano eu fiquei duas semanas
+:: tentando aprender como usar isso e misericordia velho socorro
