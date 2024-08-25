def6be57c397bee477bd094e2794c512d1c3267f
PatoFlamejanteTV <flamejantepato77@gmail.com>
2024-08-25 16:29:57 -0300

    Commit AUTOMATICO feito pelo GitSync [Ferramenta por: PatoFlamejanteTV]
    SOBRE: GitSyn e uma ferramenta criada por mim, que permite enviar e receber alteracoes de um repositorio nao-local...	\n**INFORMACOES: **Delay: 10

--- gitsync.bat
+++ gitsync.bat
@@ -3,11 +3,11 @@
 
 @echo off
 
-set delay=15
+set delay=10
 set commitmsg=Commit AUTOMATICO feito pelo GitSync [Ferramenta por: PatoFlamejanteTV]
-set descmsg=SOBRE: GitSyn e uma ferramenta criada por mim, que permite enviar e receber alteracoes de um repositório nao-local...	\n**INFORMAÇÕES: **Delay: %delay%
+set descmsg=SOBRE: GitSyn e uma ferramenta criada por mim, que permite enviar e receber alteracoes de um repositorio nao-local...	\n**INFORMACOES: **Delay: %delay%
 
-TITLE CMSG: %commitmsg% DELAY: %delay%
+TITLE CMSG: %commitmsg% DELAY: %delay% DESC: %descmsg%
 echo GitSync current configs:
 echo.
 echo DELAY: %delay%
