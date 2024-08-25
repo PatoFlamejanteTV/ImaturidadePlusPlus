55ba796d59dfd298ea75327207c3597becb586c8
PatoFlamejanteTV <flamejantepato77@gmail.com>
2024-08-25 16:57:39 -0300

    [GitSync 0.0.7] Commit AUTOMATICO feito pelo GitSync [Ferramenta por: PatoFlamejanteTV]
    SOBRE: GitSync e uma ferramenta cujo intuito e 'sincronizar' coisas de um repositorio nao-local para meu computador (e vice-versa).	GitSync Info: Version 0.0.7 , DATE (24H): 16:57.

--- gitsync.bat
+++ gitsync.bat
@@ -3,12 +3,12 @@ setlocal enableextensions
 :: GitSync, by PatoFlamejanteTV
 :: ----------------------------
 
-set VERSION=0.0.5
+set VERSION=0.0.7
 
-set delay=2
+set delay=7
 
 set commitmsg=Commit AUTOMATICO feito pelo GitSync [Ferramenta por: PatoFlamejanteTV]
-set gsabout=SOBRE: GitSync e uma ferramenta cujo intuito e 'sincronizar' coisas de um repositorio noa-local para meu computador.
+set gsabout=SOBRE: GitSync e uma ferramenta cujo intuito e 'sincronizar' coisas de um repositorio nao-local para meu computador (e vice-versa).
 
 set info=%gsabout%	GitSync Info: Version %VERSION% 
 
@@ -25,7 +25,7 @@ For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a:%%b)
 git pull
 git add *
 git push
-git commit -m "%commitmsg%" -m "%info%, DATE (24H): %mytime%."
+git commit -m "[GitSync %VERSION%] %commitmsg%" -m "%info%, DATE (24H): %mytime%."
 timeout %delay%
 cls
 goto sync
