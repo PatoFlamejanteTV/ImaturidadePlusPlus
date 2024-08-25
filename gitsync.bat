a608b54bbc886d7d742b86f891471909a1d7d6d4
PatoFlamejanteTV <flamejantepato77@gmail.com>
2024-08-25 17:08:05 -0300

    [GitSync 0.0.7] COMMIT Feito automaticamente pelo GitSync
    , DATE (24H): 17:08.

--- gitsync.bat
+++ gitsync.bat
@@ -7,7 +7,7 @@ set gs=0.0.7
 
 set delay=7
 
-set title=[GitSync %gs%]COMMIT Feito automaticamente pelo GitSync
+set title=COMMIT Feito automaticamente pelo GitSync
 echo GitSync current configs:
 echo.
 echo DELAY: %delay%
@@ -19,7 +19,7 @@ For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a:%%b)
 git pull
 git add *
 git push
-git commit -m "[GitSync %VERSION%] %title%" -m "%info%, DATE (24H): %mytime%."
+git commit -m "[GitSync %gs%] %title%" -m "%info%, DATE (24H): %mytime%."
 timeout %delay%
 cls
 goto sync
