63a4a85da23adc96d4b275649486dd9aed25b4d2
PatoFlamejanteTV <flamejantepato77@gmail.com>
2024-08-25 17:07:30 -0300

    [GitSync ] [GitSync 0.0.7]COMMIT Feito automaticamente pelo GitSync
    , DATE (24H): 17:07.

--- gitsync.bat
+++ gitsync.bat
@@ -7,12 +7,7 @@ set gs=0.0.7
 
 set delay=7
 
-set gsabout=SOBRE: GitSync e uma ferramenta cujo intuito e 'sincronizar' coisas de um repositorio nao-local para meu computador (e vice-versa).
-
-set info=%gsabout%	GitSync Info: Version %gs% 
-
-
-TITLE [GitSync %gs%] CMSG: %commitmsg% DELAY: %delay% DESC: %info%
+set title=[GitSync %gs%]COMMIT Feito automaticamente pelo GitSync
 echo GitSync current configs:
 echo.
 echo DELAY: %delay%
@@ -24,7 +19,7 @@ For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a:%%b)
 git pull
 git add *
 git push
-git commit -m "[GitSync %VERSION%] %gsabout%" -m "%info%, DATE (24H): %mytime%."
+git commit -m "[GitSync %VERSION%] %title%" -m "%info%, DATE (24H): %mytime%."
 timeout %delay%
 cls
 goto sync
