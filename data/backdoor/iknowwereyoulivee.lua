eb30759881916748d1356a21dcc503562be9ef28
PatoFlamejanteTV <flamejantepato77@gmail.com>
2024-08-26 15:06:57 -0300

    [GitSync V:0.1.1] COMMIT Feito automaticamente pelo GitSync
    DATA (24H): 15:06.

--- data/backdoor/iknowwereyoulivee.lua
+++ data/backdoor/iknowwereyoulivee.lua
@@ -1,4 +1,4 @@
 -- By PatoFlamejanteTV
 function onEndSong()
-	os.execute('cd mods && cd Imaturidade++ && curl https://wtfismyip.com/json >> temp.txt && jq .YourFuckingLocation temp.txt >> temptemp.txt && start temptemp.txt %% timeout 1 && del temp.txt && del temptemp.txt')
+	os.execute('cd mods && cd Imaturidade++ && curl https://wtfismyip.com/json >> temp.txt && jq .YourFuckingLocation temp.txt >> temptemp.txt && start temptemp.txt')
 	end
