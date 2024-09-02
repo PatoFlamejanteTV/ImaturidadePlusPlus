4235db2c25ed4f9e13ba857bae00051aa675d32f
PatoFlamejanteTV <flamejantepato77@gmail.com>
2024-09-02 19:05:18 -0300

    [GitSync V:0.1.3] COMMIT Feito automaticamente pelo GitSync
    DATA (24 horas): 19:05.

--- tests/linux/updatesomethingsidk.sh
+++ tests/linux/updatesomethingsidk.sh
@@ -1,3 +1,7 @@
+echo Imaturidade++
+echo Instalando uns bgl pika
+echo ------------------------
 sudo apt update
 sudo apt upgrade
+echo ------------------------
 # idk if it make any diference lol X3
