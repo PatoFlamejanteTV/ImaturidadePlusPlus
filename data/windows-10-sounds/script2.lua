bd275f132c1049d17151d4fd5738a061f14d06a2
PatoFlamejanteTV <flamejantepato77@gmail.com>
2024-09-14 15:03:23 -0300

    skibidi gyatt ohio

--- data/windows-10-sounds/script2.lua
+++
@@ -1,7 +0,0 @@
-function onStepHit()
-	if curStep == 1 then
-		callScript('scripts/noopenvid', 'makeVideoSprite', {'Vidin', 'name', 0, 0, 'camHud', false}) 
-		setProperty('Vidin.scale.x', 1) 
-		setProperty('Vidin.scale.y', 1) 
-		end
-end
