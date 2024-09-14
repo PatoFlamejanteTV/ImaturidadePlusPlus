5348ee54dffd83ed0432d8049b40c97eeeacd3b1
PatoFlamejanteTV <flamejantepato77@gmail.com>
2024-09-14 17:36:46 -0300

    testes sla kapakapkapk

---
+++ data/rojao/bosta.lua
@@ -0,0 +1,27 @@
+function onCreate()
+		makeLuaText('Text', "KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK", 0, 10, 10)
+        setObjectCamera('Text', 'hud')
+        setTextFont('Text', 'comic-sans-ms.ttf')
+        setTextSize('Text', 35)
+        addLuaText('Text', true)
+		
+		makeLuaText('Text2', "CAIU NO BAIT DO ROJAO AKAKAKAKAKAKAKAAKAKAK", 0, 10, 40)
+        setObjectCamera('Text2', 'hud')
+        setTextFont('Text2', 'splash.ttf')
+        setTextSize('Text2', 50)
+        addLuaText('Text2', true)
+		
+		makeLuaText('Text3', "O-T-Á-R-I-O KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK", 0, 10, 80)
+        setObjectCamera('Text3', 'hud')
+        setTextFont('Text3', 'vcr.ttf')
+        setTextSize('Text3', 80)
+        addLuaText('Text3', true)
+		
+		makeLuaText('Text4', "AGORA VEJA SEU PC EXPLODINDO LENTAMENTE", 0, 10, 140)
+        setObjectCamera('Text4', 'hud')
+        setTextFont('Text4', 'Inconsolata-Bold.ttf')
+        setTextSize('Text4', 60)
+        addLuaText('Text4', true)
+		
+		
+	end
