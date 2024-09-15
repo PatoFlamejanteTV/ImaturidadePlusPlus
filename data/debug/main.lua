a1d3b82a1b38474b3a0778abff9549be926b75ef
PatoFlamejanteTV <flamejantepato77@gmail.com>
2024-09-15 14:02:10 -0300

    gyatt skibidi ohio

---
+++ data/debug/main.lua
@@ -0,0 +1,82 @@
+--[[
+Debug test					|| Made (mainly) for Imaturidade++
+    By: PatoflamejanteTV	|| Under CC SA
+        (aka UltimateQuack)	|| 
+]]--
+
+function onCreate() -- quando começar a música
+
+	local center = 350
+	local font = "vcr.ttf"
+	local space = 40
+	
+	
+    setPropertyFromClass('flixel.FlxG', 'mouse.visible', true);
+    setPropertyFromClass('flixel.FlxG', 'drawDebug', true);
+
+    makeLuaText('Info', "Informações:", 0, center, space)
+    setObjectCamera('Info', 'hud')
+    setTextFont('Info', 'font')
+    setTextSize('Info', 35)
+    addLuaText('Info', true)
+	
+	---------------------------------------------
+	makeLuaText('BFX', "BFX", 0, center, space * 2)
+    setObjectCamera('BFX', 'hud')
+    setTextFont('BFX', 'font')
+    setTextSize('BFX', 35)
+	setTextString('BFX', "BF X POS: "..getCharacterX(bf))
+    addLuaText('BFX', true)
+	
+	---------------------------------------------
+	makeLuaText('BFY', "BFY", 0, center, space * 3)
+    setObjectCamera('BFY', 'hud')
+    setTextFont('BFY', 'font')
+    setTextSize('BFY', 35)
+	setTextString('BFY', "BF Y POS: "..getCharacterY(bf))
+    addLuaText('BFY', true)
+	
+	---------------------------------------------
+	makeLuaText('DADX', "DADX", 0, center, space * 4)
+    setObjectCamera('DADX', 'hud')
+    setTextFont('DADX', 'font')
+    setTextSize('DADX', 35)
+	setTextString('DADX', "DAD X POS: "..getCharacterX(dad))
+    addLuaText('DADX', true)
+	
+	---------------------------------------------
+	makeLuaText('DADY', "DADY", 0, center, space * 5)
+    setObjectCamera('DADY', 'hud')
+    setTextFont('DADY', 'font')
+    setTextSize('DADY', 35)
+	setTextString('DADY', "DAD Y POS: "..getCharacterY(dad))
+    addLuaText('DADY', true)
+	
+	---------------------------------------------
+	---------------------------------------------
+	makeLuaText('SPOS', "SPOS", 0, center, space * 6)
+    setObjectCamera('SPOS', 'hud')
+    setTextFont('SPOS', 'font')
+    setTextSize('SPOS', 35)
+	setTextString('SPOS', "Song Current Position: "..getSongPosition())
+    addLuaText('SPOS', true)
+	
+	---------------------------------------------
+	makeLuaText('SWidth', "SWidth", 0, center, space * 7)
+    setObjectCamera('SWidth', 'hud')
+    setTextFont('SWidth', 'font')
+    setTextSize('SWidth', 35)
+	setTextString('SWidth', "Screen width: "..getPropertyFromClass('FlxG', width))
+    addLuaText('SWidth', true)
+
+	---------------------------------------------
+	makeLuaText('SHeigth', "SHeigth", 0, center, space * 8)
+    setObjectCamera('SHeigth', 'hud')
+    setTextFont('SHeigth', 'font')
+    setTextSize('SHeigth', 35)
+	setTextString('SHeigth', "Screen heigth: "..getPropertyFromClass('FlxG', heigth))
+    addLuaText('SHeigth', true)
+
+
+	
+end
---
+++ scripts/debug.lua
@@ -0,0 +1,17 @@
+--[[
+DEBUG.LUA - Imaturidade++
+UNDER Attribution-ShareAlike 4.0 International
+
+BY: PATOFLAMEJANTETV
+]]
+function onCreate()
+print("DEBUG.LUA - Imaturidade++")
+print("BY: PATOFLAMEJANTETV")
+
+print("-------------------------")
+
+print("Screen heigth: "..getPropertyFromClass('FlxG', heigth))
+print("Screen width: "..getPropertyFromClass('FlxG', width))
+
+print("-------------------------")
+end
