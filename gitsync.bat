bfca97cd79b9159a842a5162f484e185dc9b22ae
PatoFlamejanteTV <flamejantepato77@gmail.com>
2024-09-22 13:09:37 -0300

    [GitSync V:0.1.3] COMMIT Feito automaticamente pelo GitSync
    MOTIVO: Chart com mais de 200k notas || DATA (24 horas): 13:09.

--- gitsync.bat
+++ gitsync.bat
@@ -1,6 +1,6 @@
 @echo off
 set gs=0.1.3
-set delay=5
+set delay=1
 TITLE [GitSync %gs%] Running...
 :: setlocal enableextensions
 :: GitSync, by PatoFlamejanteTV
@@ -21,7 +21,7 @@ For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a:%%b)
 git pull
 git add *
 git push
-git commit -m "[GitSync V:%gs%] %title%" -m "DATA (24 horas): %mytime%."
+git commit -m "[GitSync V:%gs%] %title%" -m "MOTIVO: Chart com mais de 200k notas || DATA (24 horas): %mytime%."
 :: timeout %delay% /nobreak
 timeout %delay%
 :: cls
