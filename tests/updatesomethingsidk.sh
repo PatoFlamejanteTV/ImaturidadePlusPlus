32f6239af8f74867ae9330093c67c11f4a5d9e0e
PatoFlamejanteTV <flamejantepato77@gmail.com>
2024-10-03 17:13:28 -0300

    organizada nums coisa ai kkkkkkkkkkkkkkkkk

--- tests/updatesomethingsidk.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo Imaturidade++
-echo Instalando uns bgl pika
-echo ------------------------
-sudo apt update
-sudo apt upgrade
-echo ------------------------
-# idk if it make any diference lol X3
